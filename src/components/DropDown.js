772d7ad85d3411d8a561ca464b3df860ae98f6cd
pieris <pieris@tuta.io>
2023-06-02 09:54:19 +0200

    divide ternary operators on multiples lines

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -3,7 +3,10 @@ import '../assets/css/DropDown.css';
 
 const DropDown = ({ id, title, className, isOpen, content }) => {
   const [isContentVisible, setIsContentVisible] = useState(isOpen);
-  const buttonIcon = isContentVisible ? '/images/icons/drop-down-open.svg' : '/images/icons/drop-down-close.svg';
+  const buttonIcon = isContentVisible
+  ? '/images/icons/drop-down-open.svg'
+  : '/images/icons/drop-down-close.svg';
+
 
   const toggleDropDown = () => {
     setIsContentVisible(!isContentVisible);
@@ -14,7 +17,13 @@ const DropDown = ({ id, title, className, isOpen, content }) => {
       <div className="drop-down-header">
         <h3>{title}</h3>
         <button className="drop-down-button" onClick={toggleDropDown}>
-          <img src={buttonIcon} alt={isContentVisible ? 'Fermer' : 'Ouvrir'} width="24" height="14" />
+          <img
+            src={buttonIcon}
+            alt={isContentVisible ? 'Fermer' : 'Ouvrir'}
+            width="24"
+            height="14"
+          />
+
         </button>
       </div>
       {isContentVisible && (
